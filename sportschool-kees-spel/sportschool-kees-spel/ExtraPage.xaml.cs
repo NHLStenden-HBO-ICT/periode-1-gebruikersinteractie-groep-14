fa57bdc7ffa0fd3b53b021f08ca8396d802b4ebd
MartijnHuisma <martijnhuisma@gmail.com>
2023-10-26 15:40:56 +0200

    Update

--- sportschool-kees-spel/sportschool-kees-spel/ExtraPage.xaml.cs
+++ sportschool-kees-spel/sportschool-kees-spel/ExtraPage.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace sportschool_kees_spel
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ExtraPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ExtraPage : Page
     {
-        public MainWindow()
+        public ExtraPage()
         {
             InitializeComponent();
         }
