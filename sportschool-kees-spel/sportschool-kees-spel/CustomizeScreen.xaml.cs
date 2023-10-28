70a75d43f01fdb971087ce2ffb70034a8a5a7f06
Imar Dros <imardros@gmail.com>
2023-10-28 18:14:55 +0200

    Update Layout navigatie && begin custom outfits

--- sportschool-kees-spel/sportschool-kees-spel/CustomizeScreen.xaml.cs
+++ sportschool-kees-spel/sportschool-kees-spel/CustomizeScreen.xaml.cs
@@ -20,28 +20,82 @@ namespace sportschool_kees_spel
     /// </summary>
     public partial class CustomizeScreen : Page
     {
+        int EquippedValue = 1;
+        int maxValue = 9;
+        int minValue = 2;
+        string EquippedValueString = string.Empty;
         public CustomizeScreen()
         {
             InitializeComponent();
+        }
 
-            int selectedOption = 0;
-
-            switch (selectedOption)
+        private void HeadwearSwitch()
+        {
+            InitializeComponent();
+            switch (EquippedValue)
             {
-                case 0:
-                    Console.WriteLine("Succes!");
+                case 1:
+                    MessageBox.Show("1");
+                    break;
+                case 2:
+                    MessageBox.Show("2");
+                    break;
+                case 3:
+                    MessageBox.Show("3");
+                    break;
+                case 4:
+                    MessageBox.Show("4");
+                    break;
+                case 5:
+                    MessageBox.Show("5");
+                    break;
+                case 6:
+                    MessageBox.Show("6");
+                    break;
+                case 7:
+                    MessageBox.Show("7");
+                    break;
+                case 8:
+                    MessageBox.Show("8");
+                    break;
+                case 9:
+                    MessageBox.Show("9");
+                    break;
+                case 10:
+                    MessageBox.Show("10");
                     break;
             }
         }
 
         private void Hoofd_Links(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Links");
+            if (EquippedValue >= minValue)
+            {
+                EquippedValue--;
+            }
+            else
+            {
+                EquippedValue = maxValue + 1;
+            }
+            EquippedValueString = EquippedValue.ToString();
+            NumberTest.Text = EquippedValueString;
+            HeadwearSwitch();
+
         }
 
         private void Hoofd_Rechts(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Rechts");
+            if (EquippedValue <= maxValue)
+            {
+                EquippedValue++;
+            }
+            else
+            {
+                EquippedValue = minValue - 1;
+            }
+            EquippedValueString = EquippedValue.ToString();
+            NumberTest.Text = EquippedValueString;
+            HeadwearSwitch();
         }
     }
 }
