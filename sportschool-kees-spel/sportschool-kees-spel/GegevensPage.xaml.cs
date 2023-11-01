d8d3932c03ef64b6430158bc8367cad614883417
Imar Dros <imardros@gmail.com>
2023-11-01 21:03:19 +0100

    Continued work

--- sportschool-kees-spel/sportschool-kees-spel/GegevensPage.xaml.cs
+++ sportschool-kees-spel/sportschool-kees-spel/GegevensPage.xaml.cs
@@ -24,5 +24,15 @@ namespace sportschool_kees_spel
         public GegevensPage()
         {
             InitializeComponent();        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void beginGame(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
--- sportschool-kees-spel/sportschool-kees-spel/MainWindow.xaml.cs
+++ sportschool-kees-spel/sportschool-kees-spel/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace sportschool_kees_spel
         public MainWindow()
         {
             InitializeComponent();
-            Games.Content = new GegevensPage(); 
+            //Games.Content = new GegevensPage(); 
         }
 
         private void Main_Navigated(object sender, NavigationEventArgs e)
